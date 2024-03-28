45c0f0c757975640222e484f6b762d5288944945
Isabel Boroni <isabelboroni@hotmail.com>
2024-03-28 16:40:23 -0300

    ignored nans and transformed 3 rows in one

--- pdf_scrapper.py
+++ pdf_scrapper.py
@@ -1,6 +1,8 @@
 import tabula
 import os
 import pandas as pd
+import math
+
 # check my tabula-py environment
 # tabula.environment_info() 
 pdf_path = os.path.join('pdf_files','fatura_picpay_fevereiro_2024.pdf')
@@ -14,25 +16,47 @@ select_columns = list(dfs[0])[:4]
 print("Selected Columns: ", select_columns)
 df_concat = pd.concat(dfs)
 df_select_columns = df_concat[select_columns] # later: select directly the first 4 columns instead of doing line 13
-df_select_columns.columns = ['data', 'descricao', 'valor', 'saldo']
+new_column_names =  ['data', 'descricao', 'valor', 'saldo']
+df_select_columns.columns = new_column_names
 head_num = 3
 result_0 = []
-for index, row in df_select_columns.head(head_num).items():
-    result_0.append({index: row})
+selected_df = df_select_columns.head(head_num)
 
-print(result_0)
+# setting new variables for the new column values for the specific row
+new_data, new_descricao, new_valor, new_saldo = '', '', '', ''
+for row in range(3):
+    for column in new_column_names:
+        value = str(selected_df[column][row])
+        # ignoring nan's and concatenating possible values in other rows
+        if value != 'nan':
+            if column == 'data':
+                new_data += value + ' '
+            elif column == 'descricao':
+                new_descricao += value + ' '
+            elif column == 'valor':
+                new_valor += value + ' '
+            elif column == 'saldo':
+                new_saldo += value + ' '
+new_data = new_data.strip()
+new_descricao = new_descricao.strip()
+new_valor = new_valor.replace('R$ ' , '').strip()
+new_saldo = new_saldo.replace('R$ ' , '').strip()
+
+new_row = (new_data, new_descricao, new_valor, new_saldo)
+print(new_row)
+# agora só falta fazer isso pro dataframe inteiro e voila!!
+
+# for row in df_select_columns.head(head_num).iterrows():
+#     for item in row
+#     print(row[1])
+#     #result_0.append(row})
+
+
 #print(df_select_columns)
 # csv_path = os.path.join('csv_files','fatura_picpay_fevereiro_2024.csv')
 # later: don't specify the name of the file, but use the same name as read in line 7
 
 # all_tables.to_csv(csv_path, sep=';', index=True, encoding='utf-8')
-
-### OBSERVACOES ###
-
-## OBS 1
-# 3 linhas do dataframe equivale a 1 linha da tabela original
-# todo: forma de mergear as 3 linhas
-# neste caso, NaN pode ser ignorado
 
 # problema, se lermos linha por linha, nao funcionara em arquivos com muitas linhas"
 
